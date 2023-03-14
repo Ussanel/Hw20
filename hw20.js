ad755e968f222ae316d394e563697dcc73dc161e
Verezub Olena <ussanel@ukr.net>
2023-03-14 21:42:52 +0200

    ad 1ex

---
+++ hw20.js
@@ -0,0 +1,12 @@
+// Завдання 1 
+let firstnumber =prompt('Enter first number');
+while(!Number(firstnumber)){
+   alert ('Please, enter you first number')
+   firstnumber = prompt('Enter first number')
+};
+
+let secondnumber =prompt('Enter second number');
+while(!Number(secondnumber)){
+   alert ('Please, enter you second number')
+   secondnumber = prompt('Enter secondnumber')
+};
